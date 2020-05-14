9b06f78b6519ae3339f6079631cc19dec5d79394
alec <alexeialvareaz4@gmail.com>
2020-05-14 10:39:41 -0600

    finished project

--- app.js
+++ app.js
@@ -5,6 +5,58 @@ const todoList = document.getElementById('todo-list');
 const filterTasks = document.getElementById('task-filter');
 
 // Functions
+
+const saveToLocaleStorage = (task) => {
+  let tasks;
+  localStorage.getItem('tasks') === null
+    ? (tasks = [])
+    : (tasks = JSON.parse(localStorage.getItem('tasks')));
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const getTask = () => {
+  let tasks;
+  localStorage.getItem('tasks') === null
+    ? (tasks = [])
+    : (tasks = JSON.parse(localStorage.getItem('tasks')));
+
+  tasks.forEach((task) => {
+    const contentTasks = document.createElement('div');
+    contentTasks.classList.add('tasks-content');
+
+    const newTask = document.createElement('li');
+    newTask.classList.add('task-item');
+    newTask.innerText = task;
+    contentTasks.appendChild(newTask);
+    //check button
+    const completeTaskBtn = document.createElement('button');
+    completeTaskBtn.classList.add('btn-complete');
+    completeTaskBtn.innerHTML = '<li class="fas fa-check"></li>';
+    contentTasks.appendChild(completeTaskBtn);
+
+    //check button
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('btn-trash');
+    trashButton.innerHTML = '<li class="fas fa-trash"></li>';
+    contentTasks.appendChild(trashButton);
+
+    todoList.appendChild(contentTasks);
+  });
+};
+
+const removeToLocaleStorage = (task) => {
+  let tasks;
+  localStorage.getItem('tasks') === null
+    ? (tasks = [])
+    : (tasks = JSON.parse(localStorage.getItem('tasks')));
+  console.log(task);
+  const i = task.children[0].innerText;
+  const position = tasks.indexOf(i);
+  tasks.splice(position, 1);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
 const addTodo = (e) => {
   e.preventDefault();
 
@@ -16,6 +68,7 @@ const addTodo = (e) => {
   newTask.innerText = todoInput.value;
   contentTasks.appendChild(newTask);
 
+  saveToLocaleStorage(todoInput.value);
   //check button
   const completeTaskBtn = document.createElement('button');
   completeTaskBtn.classList.add('btn-complete');
@@ -38,6 +91,7 @@ const deleteTask = (e) => {
   if (item.classList[0] === 'btn-trash') {
     const task = item.parentElement;
     task.classList.add('fall');
+    removeToLocaleStorage(task);
     setTimeout(() => {
       task.remove();
     }, 1000);
@@ -72,7 +126,9 @@ const filterTask = (e) => {
     }
   });
 };
+
 // Events
 todoBtn.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteTask);
 filterTasks.addEventListener('click', filterTask);
+document.addEventListener('DOMContentLoaded', getTask);
